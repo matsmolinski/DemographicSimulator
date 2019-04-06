1ab09478e7ac090d5ab283c79d9cf126ee65ddc6
smolinm1 <matsmolinski@gmail.com>
2019-04-06 23:57:39 +0200

    Adding next few classes

---
+++ DemographicSimulator/DemographicSimulator/Simulator/Map.cs
@@ -0,0 +1,16 @@
+﻿using DemographicSimulator.MapObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemographicSimulator.Simulator
+{
+    public class Map
+    { 
+        public List <Line> ContourLines { set; get; }
+        public List <River> Rivers { set; get; }
+        public List <City> Cities { set; get; }
+    }
+}
