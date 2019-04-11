cb9ac4dc0338a67fa75297d86bf7c07060032ee6
smolinm1 <matsmolinski@gmail.com>
2019-04-11 22:24:52 +0200

    Adding provisional GUI

--- DemographicSimulator/DemographicSimulator/MainWindow.Designer.cs
+++ DemographicSimulator/DemographicSimulator/MainWindow.Designer.cs
@@ -35,19 +35,27 @@
             this.addEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gamePanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cityDataBox = new System.Windows.Forms.RichTextBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.globalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
+            this.globalToolStripMenuItem,
             this.addEventToolStripMenuItem,
-            this.helpToolStripMenuItem});
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1035, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(359, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,13 +72,13 @@
             // 
             this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
             this.loadFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
-            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.loadFileToolStripMenuItem.Text = "Load file";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
             // 
@@ -91,15 +99,63 @@
             this.gamePanel.BackColor = System.Drawing.SystemColors.ControlDark;
             this.gamePanel.Location = new System.Drawing.Point(12, 31);
             this.gamePanel.Name = "gamePanel";
-            this.gamePanel.Size = new System.Drawing.Size(267, 141);
+            this.gamePanel.Size = new System.Drawing.Size(962, 604);
             this.gamePanel.TabIndex = 1;
             this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(1033, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speed";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(1017, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "01.01.2019";
+            // 
+            // cityDataBox
+            // 
+            this.cityDataBox.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.cityDataBox.Location = new System.Drawing.Point(990, 377);
+            this.cityDataBox.Name = "cityDataBox";
+            this.cityDataBox.Size = new System.Drawing.Size(180, 258);
+            this.cityDataBox.TabIndex = 4;
+            this.cityDataBox.Text = "\nWarsaw\nPopulation: 1 764 615\nAvg. temperature: 7,73 °C\nDistance to river: 0 km";
+            this.cityDataBox.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // globalToolStripMenuItem
+            // 
+            this.globalToolStripMenuItem.Name = "globalToolStripMenuItem";
+            this.globalToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.globalToolStripMenuItem.Text = "Adjust World";
+            this.globalToolStripMenuItem.Click += new System.EventHandler(this.GlobalToolStripMenuItem_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1035, 598);
+            this.ClientSize = new System.Drawing.Size(1182, 679);
+            this.Controls.Add(this.cityDataBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.gamePanel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -122,6 +178,11 @@
         private System.Windows.Forms.ToolStripMenuItem addEventToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox cityDataBox;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem globalToolStripMenuItem;
     }
 }
 
--- DemographicSimulator/DemographicSimulator/MainWindow.cs
+++ DemographicSimulator/DemographicSimulator/MainWindow.cs
@@ -13,49 +13,28 @@ namespace DemographicSimulator
     public partial class MainWindow : Form
     {
         private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.TextBox textBox1;
 
         public MainWindow()
         {
             InitializeComponent();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {this.trackBar1 });
 
-            // TextBox for TrackBar.Value update.
-            this.textBox1.Location = new System.Drawing.Point(240, 16);
-            this.textBox1.Size = new System.Drawing.Size(48, 20);
-
-            // Set up how the form should be displayed and add the controls to the form.
-            this.ClientSize = new System.Drawing.Size(296, 62);
-            this.Controls.AddRange(new System.Windows.Forms.Control[] { this.textBox1, this.trackBar1 });
-            this.Text = "TrackBar Example";
-
-            // Set up the TrackBar.
-            this.trackBar1.Location = new System.Drawing.Point(300, 300);
-            this.trackBar1.Size = new System.Drawing.Size(224, 45);
+            this.trackBar1.Location = new System.Drawing.Point(760, 80);
+            this.trackBar1.Size = new System.Drawing.Size(100, 45);
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
 
-            // The Maximum property sets the value of the track bar when
-            // the slider is all the way to the right.
             trackBar1.Maximum = 30;
-
-            // The TickFrequency property establishes how many positions
-            // are between each tick-mark.
             trackBar1.TickFrequency = 5;
-
-            // The LargeChange property sets how many positions to move
-            // if the bar is clicked on either side of the slider.
             trackBar1.LargeChange = 3;
-
-            // The SmallChange property sets how many positions to move
-            // if the keyboard arrows are used to move the slider.
             trackBar1.SmallChange = 2;
+
+            DateTime.Now.ToString("yyyy-MM-dd");
         }
 
         private void trackBar1_Scroll(object sender, System.EventArgs e)
         {
-            // Display the trackbar value in the text box.
-            textBox1.Text = "" + trackBar1.Value;
+            System.Console.WriteLine(trackBar1.Value);
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -69,6 +48,21 @@ namespace DemographicSimulator
         }
 
         private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GlobalToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
